d37693e68683eea6f330733d27a59890b9104622
Ravi Sahay <ravi.sahay27@gmail.com>
2022-09-12 20:26:44 +0530

    Reactive form validation
    Reactive form validation

--- src/app/component-fundamentals/reactive-forms-examples/reactive-forms-examples.component.ts
+++ src/app/component-fundamentals/reactive-forms-examples/reactive-forms-examples.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms-examples',
@@ -10,9 +10,42 @@ export class ReactiveFormsExamplesComponent implements OnInit {
 
   constructor() { }
 
-  username = new FormControl('',Validators.required)
+  username = new FormControl('',Validators.required);
+  password =  new FormControl('')
+
+  profile = new FormGroup({
+    firstname :new FormControl(''),
+    lastname : new FormControl(''),
+    Place : new FormControl(''),
+    mobile :new FormControl('')
+  })
+
+  educationDetails = new FormGroup({
+    qualification: new FormControl('',Validators.required),
+    passoutYear : new FormControl('',[Validators.required,Validators.maxLength(4),Validators.minLength(4)]),
+    university : new FormControl('')
+  })
 
   ngOnInit(): void {
+  }
+
+  SubmitProfile(){
+    
+    console.log(this.profile)
+    console.log("Mobile Number" + this.profile.value.mobile)
+    console.log("Place" + this.profile.value.Place)
+
+  }
+
+  SubmitEducation(){
+    this.educationDetails.markAllAsTouched();
+    if(this.educationDetails.valid){
+      console.log(this.educationDetails.value.qualification)
+      console.log(this.educationDetails.value.passoutYear)
+    }else{
+      console.log("Enter the required fields")
+    }
+   
   }
 
   update(){
@@ -20,8 +53,12 @@ export class ReactiveFormsExamplesComponent implements OnInit {
 
 
 
-    
+
   }
+
+  // studentSubmit(){
+  //   this.student.markAllAsTouched();
+  // }
 
   submit(){
     if(this.username.valid){
