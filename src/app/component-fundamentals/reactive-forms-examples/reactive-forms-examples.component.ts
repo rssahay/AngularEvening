dfd5081bb7ccea6e797a993138a79b0a3547ef7d
Ravi Sahay <ravi.sahay27@gmail.com>
2022-09-13 20:26:42 +0530

    reactive patch value
    reactive patch value

--- src/app/component-fundamentals/reactive-forms-examples/reactive-forms-examples.component.ts
+++ src/app/component-fundamentals/reactive-forms-examples/reactive-forms-examples.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { isFunction } from 'util';
 
 @Component({
   selector: 'app-reactive-forms-examples',
@@ -13,19 +14,116 @@ export class ReactiveFormsExamplesComponent implements OnInit {
   username = new FormControl('',Validators.required);
   password =  new FormControl('')
 
+  userprofile =[{
+    firstname : 'Akshay',
+    lastname : 'Kumar',
+    Place : 'Pune',
+    mobile : 745896,
+    flatno : 744,
+    street : 'qqww',
+    city: 'Pune',
+    state : 'MH'
+  },
+  {
+    firstname : 'vineet',
+    lastname : 'Kumar',
+    Place : 'Pune',
+    mobile : 745896,
+    flatno : 744,
+    street : 'qqww',
+    city: 'Pune',
+    state : 'MH'
+  },
+  {
+    firstname : 'ram',
+    lastname : 'Kumar',
+    Place : 'Pune',
+    mobile : 745896,
+    flatno : 744,
+    street : 'qqww',
+    city: 'Pune',
+    state : 'MH'
+  },
+  {
+    firstname : 'aakash',
+    lastname : 'Kumar',
+    Place : 'Pune',
+    mobile : 745896,
+    flatno : 744,
+    street : 'qqww',
+    city: 'Pune',
+    state : 'MH'
+  },
+  {
+    firstname : 'shubham',
+    lastname : 'Kumar',
+    Place : 'mumbai',
+    mobile : 745896,
+    flatno : 744,
+    street : 'qqww',
+    city: 'mumbai',
+    state : 'MH'
+  }]
+
   profile = new FormGroup({
     firstname :new FormControl(''),
     lastname : new FormControl(''),
     Place : new FormControl(''),
-    mobile :new FormControl('')
+    mobile :new FormControl(''),
+    
+    address : new FormGroup({
+      flatno : new FormControl(''),
+      street : new FormControl(''),
+      city: new FormControl(''),
+      state : new FormControl('')
+    })
+
+    // bankdetails :new FormGroup({
+    //   bankname
+    //   ifsccode
+    //   accountnumber
+    //   accounholdername
+    //   accounttype
+    // })
   })
 
   educationDetails = new FormGroup({
     qualification: new FormControl('',Validators.required),
     passoutYear : new FormControl('',[Validators.required,Validators.maxLength(4),Validators.minLength(4)]),
-    university : new FormControl('')
+    university : new FormControl(''),
+
   })
 
+  updateProfile(){
+    this.profile.patchValue({
+      firstname : 'shubham',
+      lastname : 'kumar',
+      Place : 'Pune',
+      mobile : '7444444',
+      address : {
+        city : 'mumbai'
+      }
+      // bankdetails :{
+      //   accountholdername
+        
+      // }
+    })
+
+  }
+
+  Editrecord( user :any ,index:any){
+    console.log("Row data" + user)
+    this.profile.patchValue({
+      firstname : user.firstname,
+      lastname : user.lastname,
+      address : {
+        city : user.city
+      }
+    })
+
+
+
+  }
   ngOnInit(): void {
   }
 
@@ -34,7 +132,17 @@ export class ReactiveFormsExamplesComponent implements OnInit {
     console.log(this.profile)
     console.log("Mobile Number" + this.profile.value.mobile)
     console.log("Place" + this.profile.value.Place)
-
+    this.userprofile.push({
+    firstname : this.profile.value.firstname,
+    lastname :  this.profile.value.lastname,
+    Place :  this.profile.value.Place,
+    mobile :  this.profile.value.mobile,
+    flatno : 744,
+    street : 'qqww',
+    city: 'delhi',
+    state : 'MH'
+    })
+    this.profile.reset();
   }
 
   SubmitEducation(){
@@ -49,7 +157,7 @@ export class ReactiveFormsExamplesComponent implements OnInit {
   }
 
   update(){
-    this.username.setValue('dummy')
+    this.username.setValue('mark')
 
 
 
